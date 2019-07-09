3f219eb9f7d3a154f300452cece57c4ceb73c004
Daigo Tanaka <daigo.tanaka0714@gmail.com>
2019-07-09 13:15:20 +0900

    初期設定

---
+++ src/views/Home.vue
@@ -0,0 +1,31 @@
+<template>
+  <div class="ion-page">
+    <ion-header>
+      <ion-toolbar>
+        <ion-title>Hello World</ion-title>
+      </ion-toolbar>
+    </ion-header>
+    <ion-content class="ion-padding">
+      <h1>Welcome To @ionic/vue</h1>
+      <img alt="Vue logo" src="../assets/logo.png" />
+    </ion-content>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "home",
+  methods: {
+    showModal: async function() {
+      const modal = await this.$ionic.modalController.create({
+        componentProps: {
+          propsData: {
+            userData: Math.round(Math.random() * 100)
+          }
+        }
+      });
+      await modal.present();
+    }
+  }
+};
+</script>
